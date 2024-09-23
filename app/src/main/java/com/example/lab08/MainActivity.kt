e433f966c009181590969d3d40fb9919bffd2bc1
JonathanFPJV <168221566+JonathanFPJV@users.noreply.github.com>
2024-09-23 14:40:36 -0500

    cambio de colores y creación de nuevos componentes

--- app/src/main/java/com/example/lab08/MainActivity.kt
+++ app/src/main/java/com/example/lab08/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.unit.dp
 import androidx.room.Room
 import kotlinx.coroutines.launch
 import com.example.lab08.ui.theme.Lab08Theme
+import com.example.lab08.ui.theme.create.TaskScreen
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainActivity : ComponentActivity() {
@@ -37,60 +38,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun TaskScreen(viewModel: TaskViewModel) {
-    val tasks by viewModel.tasks.collectAsState()
-    val coroutineScope = rememberCoroutineScope()
-    var newTaskDescription by remember { mutableStateOf("") }
-
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        TextField(
-            value = newTaskDescription,
-            onValueChange = { newTaskDescription = it },
-            label = { Text("Nueva tarea") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-
-        Button(
-            onClick = {
-                if (newTaskDescription.isNotEmpty()) {
-                    viewModel.addTask(newTaskDescription)
-                    newTaskDescription = ""
-                }
-            },
-            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
-        ) {
-            Text("Agregar tarea")
-        }
-
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-
-        tasks.forEach { task ->
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = task.description)
-                Button(onClick = { viewModel.toggleTaskCompletion(task) }) {
-                    Text(if (task.isCompleted) "Completada" else "Pendiente")
-                }
-            }
-        }
-
-
-        Button(
-            onClick = { coroutineScope.launch { viewModel.deleteAllTasks() } },
-            modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
-        ) {
-            Text("Eliminar todas las tareas")
-        }
-    }
-}
